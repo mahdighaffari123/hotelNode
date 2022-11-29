80f9712fd4a5cea2b3c988f1384efe4ef76f4b49
unknown <mahdi.ghaffari28@gmail.com>
2022-11-29 18:11:08 +0330

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config({ path: "./.env" });
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then((con) => console.log("connection to database was successfull"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App running on port ${port}`));
